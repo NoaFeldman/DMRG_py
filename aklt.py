486054028be23f99007b0cd1e9e57353ba3312c9
dimashev <shevchuk.dima@gmail.com>
2020-07-28 07:41:32 +0300

    start implmenting RMT method for AKLT

--- aklt.py
+++ aklt.py
@@ -24,12 +24,34 @@ sY = 1j * (np.matmul(sZ, sX) - np.matmul(sX, sZ))
 # sYsY = np.kron(sY, sY)
 # sZsZ = np.kron(sZ, sZ)
 
+
+"""A Random matrix distributed with Haar measure"""
+def haar_measure(n):
+    z = (np.random.randn(n, n) + 1j * np.random.randn(n, n)) / np.sqrt(2.0)
+    q,r = scipy.linalg.qr(z)
+    d = np.diagonal(r)
+    ph = d / np.absolute(d)
+    q = np.multiply(q, ph, q)
+    return q
+
+
 UtrBase = linalg.expm(1j * np.pi * sY)
 physicalUnifierTensor = bops.getLegsUnifierTensor(d, d)
 physicalUnifier = tn.Node(physicalUnifierTensor, backend=None)
 UtrTensor = np.tensordot(physicalUnifierTensor, UtrBase, axes=([0], [0]))
 UtrTensor = np.tensordot(UtrTensor, physicalUnifierTensor, axes=([2, 0], [0, 1]))
 Utr = tn.Node(UtrTensor, backend=None)
+
+
+def getRandomUnitary():
+    u1 = haar_measure(d)
+    u2 = haar_measure(d)
+    physicalUnifier = tn.Node(physicalUnifierTensor, backend=None)
+    UTensor = np.tensordot(physicalUnifierTensor, u1, axes=([0], [0]))
+    UTensor = np.tensordot(UTensor, u2, axes=([0], [0]))
+    UTensor = np.tensordot(UTensor, physicalUnifierTensor, axes=([1, 2], [0, 1]))
+    return tn.Node(UTensor, backend=None), u1, u2
+
 
 N = 32
 A = [i for i in range(int(N/2))]
@@ -40,23 +62,18 @@ psiP = bops.copyState(psi, conj=True)
 for i in A:
     psiP[i] = bops.permute(bops.multiContraction(psiP[i], Utr, [1], [0]), [0, 2, 1])
 
-psiDagger = bops.copyState(psi, conj=True)
-psiPDagger = bops.copyState(psiP, conj=True)
+M = 5000
+for m in range(M):
+    psiCopy = bops.copyState(psi)
+    psiPCopy = bops.copyState(psiP)
+    unitaries = [[None, None] for i in A]
+    unitariesP = [[None, None] for i in A]
+    for i in A:
+        U, u1, u2 = getRandomUnitary()
+        Up, u1p, u2p = getRandomUnitary()
+        unitaries[i] = [u1, u2]
+        unitariesP[i] = [u1p, u2p]
+        psiCopy[i] = bops.permute(bops.multiContraction(psiCopy[i], U, [1], [0]), [0, 2, 1])
+        psiPCopy[i] = bops.permute(bops.multiContraction(psiPCopy[i], Up, [1], [0]), [0, 2, 1])
+        
 
-temp = bops.multiContraction(psi[0], physicalUnifier, [1], [2])
-tempP = bops.multiContraction(psiP[0], physicalUnifier, [1], [2])
-tempD = bops.multiContraction(psiDagger[0], physicalUnifier, [1], [2])
-tempPD = bops.multiContraction(psiPDagger[0], physicalUnifier, [1], [2])
-curr = bops.multiContraction(bops.multiContraction(temp, tempD, [0, 2], [0, 2]),
-                             bops.multiContraction(tempP, tempPD, [0, 2], [0, 2]), [1, 3], [3, 1])
-# Not optimised, if D starts being big, reconsider
-for i in range(1, len(A)):
-    temp = bops.multiContraction(psi[i], physicalUnifier, [1], [2])
-    tempP = bops.multiContraction(psiP[i], physicalUnifier, [1], [2])
-    tempD = bops.multiContraction(psiDagger[i], physicalUnifier, [1], [2])
-    tempPD = bops.multiContraction(psiPDagger[i], physicalUnifier, [1], [2])
-    curr = bops.multiContraction(curr, bops.multiContraction(temp, tempD, [2], [2]), [0, 1], [0, 3])
-    curr = bops.multiContraction(curr, bops.multiContraction(temp, tempD, [2], [2]), [0, 1, 3, 5], [0, 3, 5, 2])
-curr = bops.multiContraction(bops.multiContraction(bops.multiContraction(bops.multiContraction(curr, psi[i+1], [0], [0]), psiDagger[i+1],
-                                                                  [0, 3, 4], [0, 1, 2]), psiP[i+1], [0], [0]), psiPDagger[i+1], [0, 1, 2], [0, 1, 2])
-b = 1
