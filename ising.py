2c63d61d0cfd1b6d7f16938877c3dd1d90f85826
unknown <feld.noa@gmail.com>
2020-10-26 12:21:08 +0200

    full local unitaries estimation for Ising

--- ising.py
+++ ising.py
@@ -2,6 +2,8 @@ import numpy as np
 import basicOperations as bops
 import tensornetwork as tn
 import PEPS as peps
+from matplotlib import pyplot as plt
+
 
 beta = 1
 d = 2
@@ -38,37 +40,5 @@ upRow = bops.multiContraction(cUp, dUp, '2', '0')
 downRow = bops.multiContraction(cDown, dDown, '2', '0')
 
 
-def estimateOp(xRight, xLeft, upRow, downRow, A, ops):
-    N = len(ops)
-    curr = xLeft
-    for i in range(int(N / 2)):
-        closedA = tn.Node(np.trace(bops.multiContraction(ops[i * 2], A, '1', '0').tensor, axis1=0, axis2=5))
-        closedB = tn.Node(np.trace(bops.multiContraction(ops[i * 2 + 1], A, '1', '0').tensor, axis1=0, axis2=5))
-        closed = bops.permute(bops.multiContraction(closedA, closedB, '1', '3'), [0, 3, 2, 4, 1, 5])
-        curr = bops.multiContraction(bops.multiContraction(bops.multiContraction(
-            curr, upRow, '0', '0'), closed, '023', '201', cleanOr1=True), downRow, '034', '012', cleanOr1=True)
-    return bops.multiContraction(curr, xRight, '012', '012').tensor
 
-
-l = 14
-t = estimateOp(xRight, xLeft, upRow, upRow, A, [tn.Node(np.eye(2)) for i in range(l)])
-xLeft = bops.multNode(xLeft, 1 / t)
-t = estimateOp(xRight, xLeft, upRow, upRow, A, [tn.Node(np.eye(2)) for i in range(l)])
-projector0 = np.zeros((2, 2))
-projector0[0, 0] = 1
-proj0 = tn.Node(projector0)
-projector1 = np.zeros((2, 2))
-projector1[1, 1] = 1
-proj1 = tn.Node(projector1)
-tr = 0
-for i in range(2**l):
-    ops = []
-    for b in range(l):
-        if i & 2**b == 0:
-            ops.append(proj0)
-        else:
-            ops.append(proj1)
-    p = estimateOp(xRight, xLeft, upRow, upRow, A, ops)
-    tr += p
-b = 1
 
--- randomUs.py
+++ randomUs.py
@@ -2,6 +2,8 @@ import numpy as np
 import basicOperations as bops
 import tensornetwork as tn
 import scipy
+import pickle
+import ising
 
 
 """A Random matrix distributed with Haar measure"""
@@ -29,3 +31,68 @@ def globalUnitary(N, d, numberOfLayers=2):
     return U
 
 
+def estimateOp(xRight, xLeft, upRow, downRow, A, ops):
+    N = len(ops)
+    curr = xLeft
+    for i in range(int(N / 2)):
+        closedA = tn.Node(np.trace(bops.multiContraction(ops[i * 2], A, '1', '0').tensor, axis1=0, axis2=5))
+        closedB = tn.Node(np.trace(bops.multiContraction(ops[i * 2 + 1], A, '1', '0').tensor, axis1=0, axis2=5))
+        closed = bops.permute(bops.multiContraction(closedA, closedB, '1', '3'), [0, 3, 2, 4, 1, 5])
+        curr = bops.multiContraction(bops.multiContraction(bops.multiContraction(
+            curr, upRow, '0', '0'), closed, '023', '201', cleanOr1=True), downRow, '034', '012', cleanOr1=True)
+    return bops.multiContraction(curr, xRight, '012', '012').tensor
+
+
+def localDistance(s, sp):
+    return bin(s ^ sp).count("1")
+
+
+
+def localUnitariesFull(l, M, A, xRight, xLeft, upRow, downRow, d, filename):
+    proj0Tensor = np.zeros((2, 2), dtype=complex)
+    proj0Tensor[0, 0] = 1
+    proj1Tensor = np.zeros((2, 2), dtype=complex)
+    proj1Tensor[1, 1] = 1
+    projs = [proj0Tensor, proj1Tensor]
+    avg = 0
+    avgs = []
+    for m in range(int(M * l**2)):
+        t = estimateOp(xRight, xLeft, upRow, downRow, A, [tn.Node(np.eye(2)) for i in range(l)])
+        xLeft = bops.multNode(xLeft, 1 / t)
+        us = [haar_measure(d) for i in range(l)]
+        ps = [0] * 2**l
+        purity = 0
+        for s in range(2**l):
+            currUs = [tn.Node(np.eye(d)) for i in range(l)]
+            for i in range(l):
+                currUs[i].tensor = np.matmul(np.matmul(us[i], projs[int(s & d**i > 0)]), np.conj(np.transpose(us[i])))
+            ps[s] = estimateOp(xRight, xLeft, upRow, downRow, A, currUs)
+        for s in range(2**l):
+            for sp in range(2**l):
+                purity += d**l * (-d)**(-localDistance(s, sp)) * ps[s] * ps[sp]
+        avg = (avg * m + purity) / (m + 1)
+        if m % M == M - 1:
+            avgs.append(avg)
+    with open(filename + '_l_' + str(l) + '_M_' + str(M), 'wb') as f:
+        pickle.dump(avgs, f)
+
+
+def exactPurity(d, l, xRight, xLeft, upRow, downRow, A, filename):
+    curr = xLeft
+    pair = bops.permute(bops.multiContraction(A, A, '2', '4'), [1, 6, 3, 7, 2, 8, 0, 5, 4, 9])
+    for i in range(int(l / 2)):
+        curr = bops.multiContraction(bops.multiContraction(curr, upRow, '0', '0'), downRow, '1', '0')
+        curr = bops.multiContraction(curr, pair, [0, i * 4 + 1, i * 4 + 2, i * 4 + 4, i * 4 + 5], '20145')
+        curr = bops.permute(curr, [i * 4, i * 4 + 2, i * 4 + 1] + list(range(i * 2)) + [i * 4 + 3, i * 4 + 4] +
+                            list(range(i * 2, i * 4)) + [i * 4 + 5, i * 4 + 6])
+    dm = bops.multiContraction(curr, xRight, '012', '012')
+    ordered = np.reshape(dm.tensor, [d**l, d**l]) / np.trace(np.reshape(dm.tensor, [d**l, d**l]))
+    purity =  sum(np.linalg.eigvalsh(np.matmul(ordered, ordered)))
+    with open(filename + '_l_' + str(l), 'wb') as f:
+        pickle.dump(purity, f)
+
+
+M = 1e2
+exactPurity(2, 2, ising.xRight, ising.xLeft, ising.upRow, ising.upRow, ising.A, 'exact')
+localUnitariesFull(2, M, ising.A, ising.xRight, ising.xLeft, ising.upRow, ising.upRow, 2, 'localfull')
+b = 1
