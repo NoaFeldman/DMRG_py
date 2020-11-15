121fd38bf584cc511093cdc0f8d1cc97d2c7db2c
unknown <feld.noa@gmail.com>
2020-11-15 09:05:40 +0200

    analyze results

--- analyzeResults.py
+++ analyzeResults.py
@@ -3,14 +3,29 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
+M = 100
+Ns = [4, 8, 12, 16, 20]
+
+
+
 M = 1000
-Ns = [4, 8]
-for N in Ns:
-    result = [0] * N**2
-    purity = 1 / (8 + 6 * (N - 4))
-    for m in range(N**2):
-        with open('results/toric_local_full_N_' + str(N) + '_M_' + str(M) + '_m_' + str(m * M + M - 1), 'rb') as f:
+Ns = [4, 8, 12]
+Ms = [16, 64, 36]
+expected = [1/8, 1/32, 1/128]
+legends = []
+for i in range(len(Ns)):
+    N = Ns[i]
+    result = [0] * Ms[i]
+    purity = expected[i]
+    legends.append('N = ' + str(N))
+    for m in range(Ms[i]):
+        # with open('results/toric_local_full_N_' + str(N) + '_M_' + str(M) + '_m_' + str(m * M + M - 1), 'rb') as f:
+        with open('results/toric_local_MC_N_' + str(N) + '_M_' + str(M) + '_m_' + str(m * M + M - 1) \
+                  + '_chi_100', 'rb') as f:
             result[m] = pickle.load(f)
-    plt.plot([(m * M + M - 1) / N**2 for m in range(N**2)], np.abs(np.array(result) - purity) / purity)
+    plt.plot([(m * M + M - 1) / N**2 for m in range(Ms[i])], np.abs(np.array(result) - purity) / purity)
     print(result)
+plt.xlabel(r'$M/N^2$')
+plt.ylabel(r'|$p_2$ - est|/$p_2$')
+plt.legend(legends)
 plt.show()
