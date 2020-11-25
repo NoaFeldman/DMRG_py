7af765eaa3ca8a3002ca0aa5cae1188f383cc379
unknown <feld.noa@gmail.com>
2020-11-25 21:00:50 +0200

    non unitary local ops - analyze

--- analyzeResults.py
+++ analyzeResults.py
@@ -4,43 +4,70 @@ import numpy as np
 import os.path
 import toricCode
 
-M = 100
 d = 2
-Ns = [4, 8, 12]
+
+M = 1000
+Ns = [4, 8, 12, 16, 20, 24]
 legends = []
+option = 'complex'
 for i in range(len(Ns)):
     N = Ns[i]
     spaceSize = d**N
     m = M - 1
     estimation = []
-    estimation2 = []
     legends.append('N = ' + str(N))
-    while os.path.isfile('./results/global/global_p1_N_' + str(N) + '_M_' + str(M) + '_m_' + str(m) + '_layers_4'):
-        with open('./results/global/global_p2_N_' + str(N) + '_M_' + str(M) + '_m_' + str(m) + '_layers_4', 'rb') as f:
+    while os.path.isfile('./results/' + option + '/toric_local_vecs_N_' + str(N) + '_' + option +
+                         '_M_' + str(M) + '_m_' + str(m)):
+        with open('./results/' + option + '/toric_local_vecs_N_' + str(N) + '_' + option +
+                         '_M_' + str(M) + '_m_' + str(m), 'rb') as f:
             curr = pickle.load(f)
-            estimation2.append(curr)
-        with open('./results/global/global_p1_N_' + str(N) + '_M_' + str(M) + '_m_' + str(m) + '_layers_4', 'rb') as f:
-            curr = pickle.load(f)
-            estimation.append(curr)
+            if len(estimation) == 0:
+                estimation.append(curr)
+            else:
+                estimation.append((estimation[-1] * len(estimation) + curr) / (len(estimation) + 1))
         m += M
     expected = toricCode.getPurity(i + 1)
-    if N == 12:
-        for i in range(N**2 * 4):
-            estimation.append((expected + 1) / (d**N * (d**N + 1)))
-            estimation2.append(7.39e-8 + 1e-10 * np.random.randn())
-    plt.plot([(m * M + M - 1) / (d**N) for m in range(len(estimation))],
-             (np.abs(np.array(estimation2) * (d**N) * (d**N + 1) - 1) - expected) / expected)
-    print(N)
-    print((expected + 1) / (d**N * (d**N + 1)))
-    estimation = np.array(estimation)
-    estimation2 = np.array(estimation2)
-    b = 1
-    print(np.round(estimation2, 14))
-plt.xlabel(r'$M/2^{N}$')
-plt.ylabel(r'|$p_2$ - est|/$p_2$')
-plt.title('Toric code with global unitaries - contract nearest neighbor pairs')
+    plt.plot([(m * M + M - 1) / (d ** N) for m in range(len(estimation))],
+             np.abs(np.array(estimation) - expected) / expected)
 plt.legend(legends)
 plt.show()
+
+# M = 100
+# Ns = [4, 8, 12]
+# legends = []
+# for i in range(len(Ns)):
+#     N = Ns[i]
+#     spaceSize = d**N
+#     m = M - 1
+#     estimation = []
+#     estimation2 = []
+#     legends.append('N = ' + str(N))
+#     while os.path.isfile('./results/global/global_p1_N_' + str(N) + '_M_' + str(M) + '_m_' + str(m) + '_layers_4'):
+#         with open('./results/global/global_p2_N_' + str(N) + '_M_' + str(M) + '_m_' + str(m) + '_layers_4', 'rb') as f:
+#             curr = pickle.load(f)
+#             estimation2.append(curr)
+#         with open('./results/global/global_p1_N_' + str(N) + '_M_' + str(M) + '_m_' + str(m) + '_layers_4', 'rb') as f:
+#             curr = pickle.load(f)
+#             estimation.append(curr)
+#         m += M
+#     expected = toricCode.getPurity(i + 1)
+#     if N == 12:
+#         for i in range(N**2 * 4):
+#             estimation.append((expected + 1) / (d**N * (d**N + 1)))
+#             estimation2.append(7.39e-8 + 1e-10 * np.random.randn())
+#     plt.plot([(m * M + M - 1) / (d**N) for m in range(len(estimation))],
+#              (np.abs(np.array(estimation2) * (d**N) * (d**N + 1) - 1) - expected) / expected)
+#     print(N)
+#     print((expected + 1) / (d**N * (d**N + 1)))
+#     estimation = np.array(estimation)
+#     estimation2 = np.array(estimation2)
+#     b = 1
+#     print(np.round(estimation2, 14))
+# plt.xlabel(r'$M/2^{N}$')
+# plt.ylabel(r'|$p_2$ - est|/$p_2$')
+# plt.title('Toric code with global unitaries - contract nearest neighbor pairs')
+# plt.legend(legends)
+# plt.show()
 
 
 # M = 100
