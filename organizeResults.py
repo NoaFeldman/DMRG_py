677e84d3abead3454cd3e43b8ca27a660790f0bc
feld.noa@gmail.com <feld.noa@gmail.com>
2021-01-26 15:03:22 +0200

    organize results

--- organizeResults.py
+++ organizeResults.py
@@ -1,32 +1,17 @@
 import pickle
 import numpy as np
-import os.path
+import os
 import sys
 
-d = 2
+n = sys.argv[1]
+l = sys.argv[2]
+option = sys.argv[3]
+homedir = sys.argv[4]
 
-M = 1000
-N = 4 * int(sys.argv[1])
-n = int(sys.argv[2])
-option = 'complex'
-dirs = []
-for i in range(3, len(sys.argv) - 1):
-    dirs.append(sys.argv[i])
-outdir = sys.argv[len(sys.argv) - 1]
-organized = []
-for dir in dirs:
-    m = M - 1
-    while os.path.isfile(dir + '/toric_local_vecs_N_' + str(N) + '_' + option +
-                         '_M_' + str(M) + '_m_' + str(m)):
-        with open(dir + '/toric_local_vecs_N_' + str(N) + '_' + option +
-                         '_M_' + str(M) + '_m_' + str(m), 'rb') as f:
-            organized.append(pickle.load(f))
-        m += M
-    while os.path.isfile(dir + '/toric_local_vecs_n_'+ str(n) + '_N_' + str(N) +'_' + option + '_M_' +
-                         str(M) + '_m_' + str(m)):
-        with open(dir + '/toric_local_vecs_n_'+ str(n) + '_N_' + str(N) +'_' + option + '_M_' +
-                         str(M) + '_m_' + str(m), 'rb') as f:
-            organized.append(pickle.load(f))
-        m += M
-with open(outdir + '/' + option + '_n_'+ str(n) + '_N_' + str(N) + '_' + str(len(organized)), 'wb') as f:
-    pickle.dump(organized, f)
+dir = homedir + option + n + l
+results = []
+for file in os.listdir(dir):
+    with open(dir + '/' + file, 'rb') as f:
+        results.append(pickle.load(f))
+with open(homedir + 'organized_' + option + '_' + n + '_' + l, 'wb') as f:
+    pickle.dump(results, f)
