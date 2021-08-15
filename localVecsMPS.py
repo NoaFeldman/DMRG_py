a0df1af3bbaed71305891f9c82017646f58ebace
unknown <feld.noa@gmail.com>
2021-08-15 17:11:32 +0300

    switch phase work to vectors. so we can apply the local flux op

--- localVecsMPS.py
+++ localVecsMPS.py
@@ -25,6 +25,26 @@ def localVecsEstimate(psi: List[tn.Node], vs: List[List[np.array]], option='', h
         if option == 'flux':
             for alpha in sites:
                 psiCopy[alpha] = bops.permute(bops.multiContraction(psiCopy[alpha], fourierTensor, '1', '0'), [0, 2, 1])
+
+        # dual = bops.multiContraction(psi[0], psi[1], '2', '0')
+        # triple = bops.multiContraction(dual, psi[2], '3', '0')
+        # quaple = bops.multiContraction(triple, psi[3], '4', '0')
+        # dualCopy = bops.multiContraction(psiCopy[0], psiCopy[1], '2', '0')
+        # tripleCopy = bops.multiContraction(dualCopy, psiCopy[2], '3', '0')
+        # quapleCopy = bops.multiContraction(tripleCopy, psiCopy[3], '4', '0')
+        # dm = np.reshape(bops.multiContraction(quaple, quaple, '05', '05*').tensor, [16, 16])
+        # phase = np.exp(1j * alpha)
+        # fourierFull = np.diag([phase**(-4), phase**(-2), phase**(-2), 1, phase**(-2), 1, 1, phase**2, phase**(-2),
+        #                        1, 1, phase**2, 1, phase**2, phase**2, phase**4])
+        # dmftest = np.round(np.matmul(dm, np.conj(fourierFull)), 5)
+        # dmf = np.round(np.reshape(bops.multiContraction(quaple, quapleCopy, '05', '05*').tensor, [16, 16]), 5)
+        # idx0 = np.array((3, 5, 6, 9, 10, 12)).reshape(6, 1)
+        # dm0 = dm[idx0, idx0.T]
+        # p0 = sum(np.linalg.eigh(dm0)[0] ** 3)
+        # idx2 = np.array((1, 2, 4, 8)).reshape(4, 1)
+        # dm2 = dm[idx2, idx2.T]
+        # p2 = sum(np.linalg.eigh(dm2)[0] ** 3)
+
         for alpha in sites:
             toEstimate = np.outer(vs[copy][alpha - NA], np.conj(vs[np.mod(copy + 1, n)][alpha - NA]))
             psiCopy[alpha] = bops.permute(bops.multiContraction(psiCopy[alpha], tn.Node(toEstimate), \
@@ -53,7 +73,7 @@ option = sys.argv[5]
 mydir = homedir + '/XX_MPS_NA_' + str(NA) + '_NB_' + str(NB) + '_n_' + str(n) + '_optimized'
 if option == 'flux':
     alphaInd = int(sys.argv[6])
-    alpha = np.pi * alphaInd / (2 * NA)
+    alpha = np.pi * alphaInd / NA
     mydir += '_flux_' + str(alphaInd)
     fourierOp = np.eye(2, dtype=complex)
     fourierOp[0, 0] *= np.exp(-1j * alpha)
@@ -74,15 +94,13 @@ with open(homedir + '/expected_MPS_NA_' + str(NA) + '_NB_' + str(NB) + '_n_' + s
 mySum = 0
 M = 1000
 if half == 'left':
-    steps = int(2 ** (NA * n))
+    steps = 500 # int(2 ** (NA * n))
     for k in range(len(psi) - 1, NA - 1, -1):
         psi = bops.shiftWorkingSite(psi, k, '<<')
     sites = range(NA)
 else:
     steps = int(2 ** (NB * n))
     sites = range(NA, len(psi))
-for site in sites:
-    psi[site] = bops.permute(bops.multiContraction(psi[site], U, '1', '0'), [0, 2, 1])
 for m in range(M * steps):
     if half == 'left':
         vs = [[np.array([np.exp(1j * np.pi * np.random.randint(4) / 2), np.exp(1j * np.pi * np.random.randint(4) / 2)]) \
@@ -90,6 +108,9 @@ for m in range(M * steps):
     else:
         vs = [[np.array([np.exp(1j * np.pi * np.random.randint(4) / 2), np.exp(1j * np.pi * np.random.randint(4) / 2)]) \
                for alpha in range(NB)] for copy in range(n)]
+    for copy in range(n):
+        for alpha in range(NA):
+            vs[copy][alpha] = np.matmul(U.tensor, vs[copy][alpha])
     currEstimation = localVecsEstimate(psi, vs, option=option, half=half)
     mySum += currEstimation
     if m % M == M - 1:
