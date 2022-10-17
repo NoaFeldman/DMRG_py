30490c8d6e9353a5084776a5167649c4076b4469
NoaFeldman <feld.noa@gmail.com>
2022-10-17 13:13:32 +0300

    organize N vs ising lambda

--- circle_mps.py
+++ circle_mps.py
@@ -210,37 +210,84 @@ def add_mps(psi1, psi2):
     return result
 
 
-def ground_states_magic(N, J):
-    lambda_step = 0.1
-    ising_lambdas = [np.round(lambda_step * i, 8) for i in range(1, int(1.5/lambda_step))]
+def ground_states_magic(N, J, ising_lambdas):
+    all_m2s_0_basis = np.zeros(len(ising_lambdas), dtype=complex)
+    all_m2s_min_basis = np.zeros(len(ising_lambdas), dtype=complex)
+    all_alphas_squared = np.zeros(len(ising_lambdas))
 
     psi_0 = minus_state(N)
-    for ising_lambda in ising_lambdas:
-        print(ising_lambda)
-        onsite_terms, neighbor_terms = get_H_terms(N, ising_lambda * X, J * np.kron(Z, Z))
+    for li in range(len(ising_lambdas)):
+        ising_lambda = ising_lambdas[li]
+        print(N, ising_lambda)
+        results_filename = filename('magic/results/circle_ising', J, N, ising_lambda, 'PBC')
+        if os.path.exists(results_filename):
+            [gs, state_accuracy, m2s, alpha_squared] = pickle.load(open(results_filename, 'rb'))
+        else:
+            onsite_terms, neighbor_terms = get_H_terms(N, ising_lambda * X, J * np.kron(Z, Z))
 
-        gs, E0, trunc_errs = dmrg.DMRG(psi_0, onsite_terms, neighbor_terms, d=4, initial_bond_dim=16, maxBondDim=512,
-                                       accuracy=1e-10)
-        exact_H(N, ising_lambda)
-        print(E0)
-        # split sites so it is consistent with magicRenyi.getRenyiEntropy
-        relaxed = bops.relaxState(gs, 4)
-        state_accuracy = bops.getOverlap(gs, relaxed)
+            gs, E0, trunc_errs = dmrg.DMRG(psi_0, onsite_terms, neighbor_terms, d=4, initial_bond_dim=16, maxBondDim=512,
+                                           accuracy=1e-10)
+            # split sites so it is consistent with magicRenyi.getRenyiEntropy
+            relaxed = bops.relaxState(gs, 4)
+            state_accuracy = bops.getOverlap(gs, relaxed)
+            m2s = np.zeros((angle_steps, angle_steps), dtype=complex)
+            for ti in range(angle_steps):
+                for pi in range(angle_steps):
+                    paulis = rotate_paulis(thetas[ti], phis[pi])
+                    m2 = memory_cheap_m2(relaxed, paulis)
+                    m2s[ti, pi] = m2
+            single_site_rdm = bops.contract(tn.Node(bops.contract(gs[-1], gs[-1], '02', '02*').tensor.reshape([d] * 4)),
+                                            tn.Node(np.eye(d)), '13', '01').tensor
+            print(single_site_rdm)
+            alpha_squared = sum([np.matmul(single_site_rdm, P).trace()**2 for P in [X, Y, Z]])
+            psi_0 = gs
+            pickle.dump([gs, state_accuracy, m2s, alpha_squared], open(results_filename, 'wb'))
         print(state_accuracy)
-        m2s = np.zeros((angle_steps, angle_steps), dtype=complex)
-        for ti in range(angle_steps):
-            for pi in range(angle_steps):
-                paulis = rotate_paulis(thetas[ti], phis[pi])
-                m2 = memory_cheap_m2(relaxed, paulis)
-                m2s[ti, pi] = m2
-        single_site_rdm = bops.contract(tn.Node(bops.contract(gs[-1], gs[-1], '02', '02*').tensor.reshape([d] * 4)),
-                                        tn.Node(np.eye(d)), '13', '01').tensor
-        print(single_site_rdm)
-        alpha_squared = sum([np.matmul(single_site_rdm, P).trace()**2 for P in [X, Y, Z]])
-        psi_0 = gs
-        pickle.dump([gs, state_accuracy, m2s, alpha_squared], open(filename('magic/results', J, N, ising_lambda, 'PBC'), 'wb'))
+        all_m2s_0_basis[li] = (np.log(m2s[0, 0]) - N)/ np.log(2)
+        all_m2s_min_basis[li] = (np.log(np.amin(m2s)) - N)/ np.log(2)
+        all_alphas_squared[li] = alpha_squared
+    return all_m2s_0_basis, all_m2s_min_basis, all_alphas_squared
 
-N = int(sys.argv[1])
-ground_states_magic(N, 1)
-ground_states_magic(N, -1)
 
+Ns = [i * 2 + 1 for i in range(3, 10)]
+lambda_step = 0.1
+ising_lambdas = [np.round(lambda_step * i, 8) for i in range(-int(3 / lambda_step), int(3 / lambda_step))]
+
+m2s = np.zeros((len(Ns), len(ising_lambdas)), dtype=complex)
+m2s_min = np.zeros((len(Ns), len(ising_lambdas)), dtype=complex)
+alphas_squared = np.zeros((len(Ns), len(ising_lambdas)))
+m2s_ferro = np.zeros((len(Ns), len(ising_lambdas)), dtype=complex)
+m2s_min_ferro = np.zeros((len(Ns), len(ising_lambdas)), dtype=complex)
+alphas_squared_ferro = np.zeros((len(Ns), len(ising_lambdas)))
+for Ni in range(len(Ns)):
+    N = Ns[Ni]
+    curr_m2s_0_basis, curr_m2s_min_basis, curr_alphas_squared = ground_states_magic(N, 1, ising_lambdas)
+    curr_m2s_0_basis_ferro, curr_m2s_min_basis_ferro, curr_alphas_squared_ferro = ground_states_magic(N, -1, ising_lambdas)
+    m2s[Ni, :] = curr_m2s_0_basis
+    m2s_min[Ni, :] = curr_m2s_min_basis
+    alphas_squared[Ni, :] = curr_alphas_squared
+    m2s_ferro[Ni, :] = curr_m2s_0_basis_ferro
+    m2s_min_ferro[Ni, :] = curr_m2s_min_basis_ferro
+    alphas_squared_ferro[Ni, :] = curr_alphas_squared_ferro
+
+plot = False
+if plot:
+    import matplotlib.pyplot as plt
+    ff, axs = plt.subplots(2, 3)
+    m = axs[0, 0].pcolormesh(ising_lambdas, Ns, np.real(m2s))
+    plt.colorbar(m, ax=axs[0, 0])
+    m = axs[0, 1].pcolormesh(ising_lambdas, Ns, np.real(m2s_min))
+    plt.colorbar(m, ax=axs[0, 1])
+    m = axs[0, 2].pcolormesh(ising_lambdas, Ns, np.real(alphas_squared))
+    plt.colorbar(m, ax=axs[0, 2])
+    m = axs[1, 0].pcolormesh(ising_lambdas, Ns, np.real(m2s_ferro))
+    plt.colorbar(m, ax=axs[1, 0])
+    m = axs[1, 1].pcolormesh(ising_lambdas, Ns, np.real(m2s_min_ferro))
+    plt.colorbar(m, ax=axs[1, 1])
+    m = axs[1, 2].pcolormesh(ising_lambdas, Ns, np.real(alphas_squared_ferro))
+    plt.colorbar(m, ax=axs[1, 2])
+    for i in range(2):
+        for j in range(3):
+            axs[i, j].set_xlabel(r'$\lambda$')
+            axs[i, j].set_ylabel('N')
+    plt.show()
